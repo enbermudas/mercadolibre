e369d29b644d20f982bd8ab3418989c5c9080103
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-18 19:40:49 -0300

    feat: create items endpoint

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import controller from './controller';
+import validator from './validator';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).send({ status: 'online' });
 });
+
+router.get('/items', validator.items, controller.items);
 
 export default router;
