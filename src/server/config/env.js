7b4ff30b2eb288169120a65342f8f1db3c124b18
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-18 20:09:20 -0300

    feat: update env config consts

--- src/server/config/env.js
+++ src/server/config/env.js
@@ -2,4 +2,10 @@ import dotenv from 'dotenv-safe';
 
 dotenv.config();
 
-export const { NODE_ENV, HOST, PORT, LOG_LEVEL, MORGAN_FORMAT } = process.env;
+export const {
+  NODE_ENV,
+  SERVER_HOST,
+  SERVER_PORT,
+  LOG_LEVEL,
+  MORGAN_FORMAT
+} = process.env;
