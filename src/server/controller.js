5f6b7588062249ba76e6c435b9027e3efd7dab6a
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-18 19:40:01 -0300

    feat: create controller with items method

---
+++ src/server/controller.js
@@ -0,0 +1,48 @@
+import { validationResult } from 'express-validator';
+import http from './config/http';
+
+export default {
+  items: async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).send({
+        errors: errors.array()
+      });
+    }
+
+    try {
+      const { data } = await http.get('/sites/MLA/search', {
+        params: { q: req.query.q }
+      });
+
+      let items = [];
+      let categories = [];
+
+      if (data.results.length !== 0) {
+        data.results.map((item) => ({
+          id: item.id,
+          title: item.title,
+          price: {
+            currency: item.currency_id,
+            amount: item.installments.amount,
+            decimals: 2
+          },
+          picture: item.thumbnail,
+          condition: item.condition,
+          free_shipping: item.shipping.free_shipping,
+          address: item.address.state_name
+        }));
+      }
+
+      if (data.filters.length !== 0) {
+        categories = data.filters
+          .find((filter) => filter.id === 'category')
+          .values[0].path_from_root.map((crumb) => crumb.name);
+      }
+
+      return res.status(200).send({ items, categories });
+    } catch (error) {
+      return res.status(500).send({ error: error.message });
+    }
+  }
+};
