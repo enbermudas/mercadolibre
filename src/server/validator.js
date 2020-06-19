76a93d08742484d67c53d82fa839569bd2f32df7
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-18 21:22:38 -0300

    feat: add item detail validator

--- src/server/validator.js
+++ src/server/validator.js
@@ -1,5 +1,6 @@
-import { query } from 'express-validator';
+import { param, query } from 'express-validator';
 
 export default {
-  items: [query('q').not().isEmpty().withMessage('Query must be defined.')]
+  items: [query('q').not().isEmpty().withMessage('Query must be defined.')],
+  item: [param('id').not().isEmpty().withMessage('ID must be defined.')]
 };
