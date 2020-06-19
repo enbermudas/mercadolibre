e4122e22adfe3a4971b15dac743ad7483ba38639
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-19 19:28:08 -0300

    feat: add categories component to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { MeliProvider } from './store';
 import './App.scss';
@@ -9,29 +10,45 @@ import List from './views/List';
 
 // components
 import NavBar from './components/NavBar';
+import Categories from './components/Categories';
+
+const Main = styled.main.attrs({
+  'data-testid': 'main',
+  role: 'main'
+})`
+  width: 100%;
+  height: auto;
+  display: block;
+  padding: 0 30px;
+`;
 
 const App = () => {
   const [query, setQuery] = useState('');
   const [list, setList] = useState({ categories: [], items: [] });
+  const { categories } = list;
 
   return (
     <MeliProvider value={{ query, setQuery, list, setList }}>
       <Router>
         <NavBar />
 
-        <Switch>
-          <Route exact path="/items">
-            <List />
-          </Route>
+        <Main>
+          {categories.length > 0 && <Categories />}
 
-          <Route path="/items/:id">
-            <Detail />
-          </Route>
+          <Switch>
+            <Route exact path="/items">
+              <List />
+            </Route>
 
-          <Route path="*">
-            <List />
-          </Route>
-        </Switch>
+            <Route path="/items/:id">
+              <Detail />
+            </Route>
+
+            <Route path="*">
+              <List />
+            </Route>
+          </Switch>
+        </Main>
       </Router>
     </MeliProvider>
   );
