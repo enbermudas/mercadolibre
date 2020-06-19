cb8a4bd846c849cce7edccc1bab84b82c451aa64
Enrique Bermúdez <enbermudez96@gmail.com>
2020-06-18 21:59:31 -0300

    feat: add routing to app main component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Redirect, Switch, Route } from 'react-router-dom';
+
+// components
+import Detail from './components/Detail';
+import List from './components/List';
 
 const App = () => {
-  return <h1>MercadoLibre - Frontend test project.</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/items">
+          <List />
+        </Route>
+
+        <Route path="/items/:id">
+          <Detail />
+        </Route>
+
+        <Redirect from="/" to={{ pathname: '/items' }}>
+          <List />
+        </Redirect>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
